8d2d362d848406bae93c8d640e3db8f224e3f65b
md-suhag <abdussalamsuhag104@gmail.com>
2025-11-16 15:55:06 +0600

    add: defined sales repo model and interface

---
+++ src/app/modules/salesRep/salesRep.route.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { USER_ROLES } from "../../../enums/user";
+import auth from "../../middlewares/auth";
+
+const router = express.Router();
+
+export const SalesRepRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,22 +1,23 @@
-import express from 'express';
-import { UserRoutes } from '../modules/user/user.routes';
-import { AuthRoutes } from '../modules/auth/auth.routes';
+import express from "express";
+import { UserRoutes } from "../modules/user/user.routes";
+import { AuthRoutes } from "../modules/auth/auth.routes";
 // import { PostRoutes } from '../modules/blogpost/post.route';
-import { UserManagementRoutes } from '../modules/userManagement/usermanagement.routes';
+import { UserManagementRoutes } from "../modules/userManagement/usermanagement.routes";
 // import { MatchingRoutes } from '../modules/matching/matching.routes';
 // import { ChatRoutes } from '../modules/chat/chat.route';
 // import { GameRoutes } from '../modules/game/game.routes';
 // import { EventRoutes } from '../modules/event/event.route';
-import { PromoRoutes } from '../modules/promotionAdmin/promotionAdmin.routes';
-import { RuleRoutes } from '../modules/rule/rule.route';
+import { PromoRoutes } from "../modules/promotionAdmin/promotionAdmin.routes";
+import { RuleRoutes } from "../modules/rule/rule.route";
 // import { ShopRoutes } from '../modules/shopManagement/shop.routes';
 // import { AurashopRoute } from '../modules/shopAuraSubscription/aurashop.routes';
 // import { ServerHealthRoutes } from '../modules/analytics-real-time-server/analytic.route';
-import { DashboardRoutes } from '../modules/dashbordOverview/dashboard.routes';
-import { SubscriptionRoutes } from '../modules/subscription/subscription.routes';
-import { PackageRoutes } from '../modules/package/package.routes';
-import { PromoMercentRoutes } from '../modules/promotionMercent/promotionMercent.routes';
-import { TierRoutes } from '../modules/point&TierSystem/tier.route';
+import { DashboardRoutes } from "../modules/dashbordOverview/dashboard.routes";
+import { SubscriptionRoutes } from "../modules/subscription/subscription.routes";
+import { PackageRoutes } from "../modules/package/package.routes";
+import { PromoMercentRoutes } from "../modules/promotionMercent/promotionMercent.routes";
+import { TierRoutes } from "../modules/point&TierSystem/tier.route";
+import { SalesRepRoutes } from "../modules/salesRep/salesRep.route";
 // import { AuraBundleRoute } from '../modules/shopAuroBundle/auraBundle.routes';
 // import { BuyCall } from '../modules/buy/buy.routes';
 // import { CallBundleRoute } from '../modules/shopCallBundle/callBundle.routes';
@@ -34,42 +35,37 @@ import { TierRoutes } from '../modules/point&TierSystem/tier.route';
 const router = express.Router();
 
 const apiRoutes = [
-    { path: "/user", route: UserRoutes },
-    { path: "/auth", route: AuthRoutes },
-    {path: "/usermanagement", route:UserManagementRoutes},
-    { path: "/overview", route: DashboardRoutes },
-    { path: "/subscription", route: SubscriptionRoutes },
-    {path: "/rule", route:RuleRoutes},
-    {path: "/promo", route:PromoRoutes},
-    {path: "/promomercent", route:PromoMercentRoutes},
-    {path: "/tier", route:TierRoutes},
-    {path: "/package", route:PackageRoutes},
-    // {path: "/chat", route:ChatRoutes},
-    // {path: "/game", route:GameRoutes},
-    // {path: "/event", route:EventRoutes},
-    // {path: "/shop", route:ShopRoutes},
-    // {path: "/aurabundle", route:AuraBundleRoute},
-    // {path: "/aura", route:AurashopRoute},
-    // {path: "/server", route:ServerHealthRoutes},
-    // {path: "/matching", route:MatchingRoutes},
-    // { path: "/callpurchase", route: BuyCall },
-    // { path: "/callbundle", route: CallBundleRoute },
-    // { path: "/report", route: ReportRoutes },
+  { path: "/user", route: UserRoutes },
+  { path: "/auth", route: AuthRoutes },
+  { path: "/usermanagement", route: UserManagementRoutes },
+  { path: "/overview", route: DashboardRoutes },
+  { path: "/subscription", route: SubscriptionRoutes },
+  { path: "/rule", route: RuleRoutes },
+  { path: "/promo", route: PromoRoutes },
+  { path: "/promomercent", route: PromoMercentRoutes },
+  { path: "/tier", route: TierRoutes },
+  { path: "/package", route: PackageRoutes },
+  { path: "/sales-rep", route: SalesRepRoutes },
+  // {path: "/chat", route:ChatRoutes},
+  // {path: "/game", route:GameRoutes},
+  // {path: "/event", route:EventRoutes},
+  // {path: "/shop", route:ShopRoutes},
+  // {path: "/aurabundle", route:AuraBundleRoute},
+  // {path: "/aura", route:AurashopRoute},
+  // {path: "/server", route:ServerHealthRoutes},
+  // {path: "/matching", route:MatchingRoutes},
+  // { path: "/callpurchase", route: BuyCall },
+  // { path: "/callbundle", route: CallBundleRoute },
+  // { path: "/report", route: ReportRoutes },
 
+  // { path: "/package", route: PackageRoutes },
+  // { path: "/subscription", route: SubscriptionRoutes },
+  // { path: "/role", route: RoleRoutes },
+  // { path: "/usermanage", route: UserManagementRoutes },
+  // { path: "/notification", route: NotificationRoutes },
+  // { path: "/analytic", route:  AnalyticRoutes },
+  // {path: "/user-subscription", route: UserSubscriptionRoutes},  // new user subscription route added
+];
 
-
-
-
-
-    // { path: "/package", route: PackageRoutes },
-    // { path: "/subscription", route: SubscriptionRoutes },
-    // { path: "/role", route: RoleRoutes },
-    // { path: "/usermanage", route: UserManagementRoutes },
-    // { path: "/notification", route: NotificationRoutes },
-    // { path: "/analytic", route:  AnalyticRoutes },
-    // {path: "/user-subscription", route: UserSubscriptionRoutes},  // new user subscription route added
-
-]
-
-apiRoutes.forEach(route => router.use(route.path, route.route));
+apiRoutes.forEach((route) => router.use(route.path, route.route));
 export default router;
